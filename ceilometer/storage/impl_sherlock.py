49f5b179a9a886fcbff639b79bb450ef57aad529
mizeng <mizeng@ebay.com>
2014-09-30 14:05:47 +0800

    use ceilometer.conf.connection to inject sherlock url

--- ceilometer/storage/impl_sherlock.py
+++ ceilometer/storage/impl_sherlock.py
@@ -27,6 +27,7 @@ from ceilometer.storage import base
 from ceilometer.storage import models
 from ceilometer.storage import pymongo_base
 from datetime import datetime
+import six.moves.urllib.parse as urlparse
 import time
 import json
 
@@ -49,7 +50,6 @@ def send_get_data_points(parsed_url, limit=None):
             return data_points[data_points_length-limit:]
         return data_points
 
-
 def send_get_tags(parsed_url):
     headers = {'content-type': 'application/json', 'accept': 'application/json'}
     r = requests.get(parsed_url, headers=headers)
@@ -59,6 +59,71 @@ def send_get_tags(parsed_url):
     tags = r.json()[0]['Tags']
     return tags
 
+def make_timestamp_range(start, end,
+                         start_timestamp_op=None, end_timestamp_op=None):
+
+    """Given two possible datetimes and their operations, create the query
+    document to find timestamps within that range.
+    By default, using $gte for the lower bound and $lt for the
+    upper bound.
+    """
+    ts_range = {}
+
+    if start:
+        if start_timestamp_op == 'gt':
+            start_timestamp_op = '$gt'
+        else:
+            start_timestamp_op = '$gte'
+        ts_range[start_timestamp_op] = start
+
+    if end:
+        if end_timestamp_op == 'le':
+            end_timestamp_op = '$lte'
+        else:
+            end_timestamp_op = '$lt'
+        ts_range[end_timestamp_op] = end
+    return ts_range
+
+
+def make_query_from_filter(sample_filter, require_meter=True):
+    """Return a query dictionary based on the settings in the filter.
+
+    :param filter: SampleFilter instance
+    :param require_meter: If true and the filter does not have a meter,
+                          raise an error.
+    """
+    q = {}
+
+    if sample_filter.user:
+        q['user_id'] = sample_filter.user
+    if sample_filter.project:
+        q['project_id'] = sample_filter.project
+
+    if sample_filter.meter:
+        q['counter_name'] = sample_filter.meter
+    elif require_meter:
+        raise RuntimeError('Missing required meter specifier')
+
+    ts_range = make_timestamp_range(sample_filter.start,
+                                    sample_filter.end,
+                                    sample_filter.start_timestamp_op,
+                                    sample_filter.end_timestamp_op)
+
+    if ts_range:
+        q['timestamp'] = ts_range
+
+    if sample_filter.resource:
+        q['resource_id'] = sample_filter.resource
+    if sample_filter.source:
+        q['source'] = sample_filter.source
+    if sample_filter.message_id:
+        q['message_id'] = sample_filter.message_id
+
+    # so the samples call metadata resource_metadata, so we convert
+    # to that.
+    q.update(dict(('resource_%s' % k, v)
+                  for (k, v) in sample_filter.metaquery.iteritems()))
+    return q
 
 class SherlockStorage(base.StorageEngine):
     """
@@ -99,12 +164,17 @@ class Connection(base.Connection):
         # db2 driver from mongodb driver be replaced so that pymongo will not
         # produce an exception on the scheme.
         url = conf.database.connection
-        url = url.replace('sherlock:', 'mongodb:', 1)
-        self.conn = self.CONNECTION_POOL.connect(url)
+        mongo_url = url.replace('sherlock:', 'mongodb:', 1).split('#',1)[0]
+        self.conn = self.CONNECTION_POOL.connect(mongo_url)
 
-        connection_options = pymongo.uri_parser.parse_uri(url)
+        connection_options = pymongo.uri_parser.parse_uri(mongo_url)
         self.mongo_host =  connection_options['nodelist'][0][0]
         self.db = getattr(self.conn, connection_options['database'])
+
+        # Note that url is usually consist of 2 section: mongodb and sherlock
+        # Sample:connection=sherlock://ceilometer:ceilometer_pass@10.9.205.221:27017/ceilometer\
+        # #http://cmevpsqry.vip.stratus.slc.ebay.com:8080/q?
+        self.sherlock_url = urlparse.urlparse(url).fragment
 
     def upgrade(self):
         pass
@@ -258,7 +328,73 @@ class Connection(base.Connection):
                   'min': min_value, 'max': max_value, 'sum': sum_value, 'avg': avg_value, 'period': period, 'unit': unit}
         return json.dumps(result)
 
+    def _retrieve_samples(self, query, limit):
+        # Notice that counter_name mapping to meter.counter_name in Resource Mongo Collection
+        # We need to query Resource Collection to get all the info expert counter_volume and timestamp
+        # which we will get in Sherlock
+        query_for_resource = {}
+        if query.has_key('counter_name'):
+           query_for_resource['meter.counter_name'] = query['counter_name']
 
+        resource_content = self.db.resource.find(query_for_resource)
+        samples = []
+
+        for r in resource_content:
+            user_id = r['user_id']
+            resource_id = r['_id']
+            project_id = r['project_id']
+            message_id = r.get('message_id','')
+            source = r['source']
+            resource_metadata = r['metadata']
+
+            # Get counter volume and timestamp from sherlock
+            # Below Section is to format query sherlock as user requested
+            # interval : recommend 60s or 300s or 600s
+            start = '2014-09-12T12:00:00'
+            start_timestamp = int(time.mktime(time.strptime(start,'%Y-%m-%dT%H:%M:%S')))
+            end = '2014-09-12T13:00:00'
+            end_timestamp = int(time.mktime(time.strptime(end,'%Y-%m-%dT%H:%M:%S')))
+            aggregator = 'avg'
+            metricName = 'stratus.sys.cpu_util'
+            interval = '600s'
+            # groupBy = 'fqdn=*'+',c3CeilometerResourceId={0}'.format(resource_id)
+            groupBy = 'fqdn=*'+',c3CeilometerResourceId={0}'.format('aad43446-4018-44ac-8423-1d4eec5fd0fd')
+            url = self.sherlock_url+'start={0}&end={1}&m={2}:{3}.STATE.{4}{{{5}}}&format=json'\
+            .format(start_timestamp, end_timestamp, aggregator, metricName, interval, groupBy)
+            data_points = send_get_data_points(url, limit=limit)
+            for data_point in data_points:
+                counter_volume = data_point[1]
+                timestamp = datetime.fromtimestamp(int (data_point[0]))
+                for i_meter in r['meter']:
+                    if i_meter['counter_name'] == query['counter_name']:
+                        counter_unit = i_meter.get('counter_unit', '')
+                        counter_name = i_meter['counter_name']
+                        counter_type = i_meter['counter_type']
+                        sample = {'counter_name': counter_name, 'user_id': user_id, 'resource_id': resource_id, 'timestamp': timestamp,\
+                          'message_id': message_id, 'source': source, 'counter_unit': counter_unit, 'counter_volume': counter_volume, \
+                          'project_id': project_id, 'counter_type': counter_type, 'resource_metadata': resource_metadata,\
+                          'recorded_at': timestamp}
+                        samples.append(sample)
+
+        for s in samples:
+            # Backward compatibility for samples without units
+            # s['counter_unit'] = s.get('counter_unit', '')
+            # Tolerate absence of recorded_at in older datapoints
+            s['message_signature'] = s.get('message_signature', '')
+            yield models.Sample(**s)
+
+    def get_samples(self, sample_filter, limit=5):
+        """Return an iterable of model.Sample instances.
+
+        :param sample_filter: Filter.
+        :param limit: Maximum number of items per resource to return. default set to 5
+        """
+        if limit == 0:
+            return []
+        q = make_query_from_filter(sample_filter,
+                                   require_meter=False)
+
+        return self._retrieve_samples(q,limit)
 
 
 # if __name__ == "__main__":
