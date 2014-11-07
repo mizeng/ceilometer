e8d0243bb6037f2665ecd071bcdf7f8a8ad0fad0
lzhijun <lzhijun@ebay.com>
2014-11-07 14:56:41 +0800

    redirect infra log to target log file

--- ceilometer_ebay/sherlock/sherlock_client.py
+++ ceilometer_ebay/sherlock/sherlock_client.py
@@ -74,6 +74,8 @@ class SherlockClient(object):
         self.maxsize = sherlock_config.maxsize
         self.log_level = sherlock_config.log_level
         infra.ll.set_log_level(self.log_level)
+        #infra.ll.use_the_file(log._get_log_file_path())
+        infra.ll.use_the_file('ceilometer-sherlock-request.log')
         self.frontier = infra.contrib.frontier.Frontier(host=self.host, port=self.port,
                                                             tenant=self.tenant,
                                                             env=self.env,
